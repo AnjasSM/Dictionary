861e82f7cfa3e5bf0746191f2f12fc36873c8b89
AnjasSM <syarifudin.anjas@gmail.com>
2019-07-19 16:37:45 +0700

    ini commit

--- Dictionary.js
+++ Dictionary.js
@@ -1,19 +1,16 @@
-let input = 'programit';
+let input = 'programmerit';
 
 const dictionary = (param) => {
     let arr = ['pro', 'gram', 'merit', 'program', 'it', 'programmer']
     let output = ''
+    let finalOutput = ''
     let str = param
     
-    for (i = 0; i < param.length; i++) {
+    for (i = 0; i < arr.length; i++) {
         let deleteComma = output.replace(/,/g, '');
 
         if(str.includes(arr[i])) {
-            if(deleteComma == arr[i]) {
-                output += ''
-            } else {
-                output += `${arr[i]},`
-            }
+            output += `${arr[i]},`
         }
     }
 
@@ -22,3 +19,4 @@ const dictionary = (param) => {
 
 
 console.log(dictionary(input))
+
